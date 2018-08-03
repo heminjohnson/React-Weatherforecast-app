a45b7df7d8881a744a71837d270fbd450120f0f3
Hemin Johnson <hemin.johnson@mieterengel.de>
2018-08-04 01:50:17 +0200

    feat: call the action creator when the user clicks on the submit button

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,20 +3,23 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 import {fetchWeather} from '../actions/index'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor (props) {
     super(props)
     this.state = {term: ''}
     this.onInputChange = this.onInputChange.bind(this)
+    this.onFormSubmit = this.onFormSubmit.bind(this)
   }
 
   onInputChange (event) {
-    console.log(event.target.value)
     this.setState({term: event.target.value})
   }
 
   onFormSubmit (event) {
     event.preventDefault()
+
+    this.props.fetchWeather(this.state.term)
+    this.setState({term: ''})
   }
   render () {
     return (
